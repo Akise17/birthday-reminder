5af87f86cbb996667f365917ab2960ae9305e9d5
Ahmad Fauzan <akisearu17@192.168.1.7>
2023-02-25 01:50:24 +0800

    setting up sidekiq scheduler cronjob

---
+++ app/sidekiq/reminder_job.rb
@@ -0,0 +1,12 @@
+require 'sidekiq-scheduler'
+
+class ReminderJob
+  include Sidekiq::Worker
+  def perform
+    users = User.where('EXTRACT(MONTH FROM birthday_date) = ?', DateTime.now.month)
+                .where('EXTRACT(DAY FROM birthday_date) = ?', DateTime.now.day)
+    users.each do |user|
+      UserMailer.with(user:).happy_birthday.deliver_later
+    end
+  end
+end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,5 @@
 require 'sidekiq'
+require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
@@ -7,3 +8,10 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
+
+# Sidekiq.configure_server do |config|
+#   config.on(:startup) do
+#     Sidekiq.schedule = YAML.load_file(File.expand_path('../sidekiq.yml', __FILE__))
+#     SidekiqScheduler::Scheduler.instance.reload_schedule!
+#   end
+# end
